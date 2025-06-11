660eae1d7dbb321b932c214f66a25fa460850e68
CatScan-crypt <mariroth60@gmail.com>
2025-06-11 15:03:30 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { NotificationHandler } from './components/NotificationHandler'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <NotificationHandler />
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
--- vite.config.ts
+++ vite.config.ts
@@ -1,35 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.js',
-      devOptions: {
-        enabled: true,
-      },
-      manifest: {
-        name: "PN Application",
-        short_name: "PN",
-        start_url: "/",
-        display: "standalone",
-        background_color: "#ffffff",
-        theme_color: "#000000",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png"
-          }
-        ]
-      },
-    })
   ],
   server: {
     headers: {
