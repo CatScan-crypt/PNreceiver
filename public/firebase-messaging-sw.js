011146bb9224b7da170e0d7a14b675b19b5342ce
CatScan-crypt <mariroth60@gmail.com>
2025-06-16 22:43:02 +0300

    Test logger.

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -19,3 +19,29 @@ firebase.initializeApp(firebaseConfig);
 // Retrieve Firebase Messaging instance
 const messaging = firebase.messaging();
 
+// Add notification click event listener
+self.addEventListener('notificationclick', (event) => {
+  console.log('Notification was clicked!', event);
+  
+  // Get the notification data
+  const notificationData = event.notification.data.FCM_MSG?.data || {};
+  console.log('Notification data:', notificationData);
+  
+  // Close the notification
+  event.notification.close();
+  
+  // This looks to see if the current is already open and focuses if it is
+  event.waitUntil(
+    clients.matchAll({ type: 'window' }).then((clientList) => {
+      for (const client of clientList) {
+        if (client.url === '/' && 'focus' in client) {
+          return client.focus();
+        }
+      }
+      if (clients.openWindow) {
+        return clients.openWindow('/');
+      }
+    })
+  );
+});
+
