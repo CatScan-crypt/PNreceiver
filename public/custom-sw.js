752dd25ad5d2a170fcd25f3ab6b40a62f73438e7
CatScan-crypt <mariroth60@gmail.com>
2025-06-19 16:30:11 +0300

    t2

--- public/custom-sw.js
+++ public/custom-sw.js
@@ -31,3 +31,40 @@ messaging.onBackgroundMessage(function(payload) {
 
   self.registration.showNotification(notificationTitle, notificationOptions);
 });
+
+// Handle notification click
+self.addEventListener('notificationclick', (event) => {
+  console.log('Notification clicked');
+  console.log('payload', event);
+  event.notification.close();
+
+  if (event.action) {
+    // Handle custom actions if any
+    console.log('Action clicked:', event.action);
+    
+  } else {
+    // Default click behavior
+    const urlToOpen = event.notification.data?.link || '/';
+    
+    event.waitUntil(
+      clients.matchAll({ type: 'window', includeUncontrolled: true }).then((clientList) => {
+        // Check if any window is already open with the target URL
+        const existingClient = clientList.find(client => 
+          client.url.includes(urlToOpen) && 'focus' in client
+        );
+        
+        if (existingClient) {
+          return existingClient.focus();
+        }
+        
+        // If no window is open, open a new one
+        return clients.openWindow(urlToOpen);
+      })
+    );
+  }
+});
+
+// Handle notification close
+self.addEventListener('notificationclose', (event) => {
+    console.log('Notification closed:', event.notification.tag);
+  });
